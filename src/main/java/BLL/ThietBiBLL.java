ea0f9ec314438cf1242adf86b685788eb3013558
Huchuynh <tuanhaomach@gmail.com>
2024-04-03 14:38:03 +0700

    feat: bo sung package BLL

---
+++ src/main/java/BLL/ThietBiBLL.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BLL;
+
+import DAL.ThietBiDAL;
+import java.util.List;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ThietBiBLL {
+    private ThietBiDAL ThietBiDAL;
+
+    public ThietBiBLL() {
+        ThietBiDAL = new ThietBiDAL();
+    }
+
+    public List loadThanhVien() {
+        List list;
+        list = ThietBiDAL.loadThietBi();
+        return list;
+    }
+}
