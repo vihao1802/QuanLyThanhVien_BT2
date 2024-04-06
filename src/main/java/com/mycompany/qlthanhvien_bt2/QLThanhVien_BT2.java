c54263b4048d8fa1f66e09b3ed17ad9edd27fc9c
Dikay3105 <huhuhuhuvbd@gmail.com>
2024-04-06 14:25:38 +0700

    up

--- src/main/java/com/mycompany/qlthanhvien_bt2/QLThanhVien_BT2.java
+++ src/main/java/com/mycompany/qlthanhvien_bt2/QLThanhVien_BT2.java
@@ -5,6 +5,7 @@
 package com.mycompany.qlthanhvien_bt2;
 import DAL.ThanhVien;
 import DAL.ThanhVienDAL;
+import GUI.MainForm;
 import java.util.List;
 
 /**
@@ -19,5 +20,6 @@ public class QLThanhVien_BT2 {
         for (ThanhVien tv : tvlist) {
             System.out.print(tv.getListXuLy()+ "\n");
         }
+        new MainForm();
     }
 }
